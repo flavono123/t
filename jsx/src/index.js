90edd3f444bb5dd89cd645aec088921089cfeeb5
Hansuk Hong <flavono123@gmail.com>
2019-02-14 14:02:37 +0900

    Modern React with Redux; ch2
    Signed-off-by: Hansuk Hong <flavono123@gmail.com>

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,7 +4,20 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-  return <div>Hi there!</div>;
+  const button = {text: 'Click me!'};
+  const labelText = 'Example:';
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{backgroundColor: 'blue', color: 'white'}}>
+        {button.text}
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
